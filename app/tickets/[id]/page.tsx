0844b5827fd6de012fb592a6f797f1d9679d2955
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-06-16 23:50:48 +0500

    tickets

---
+++ app/tickets/[id]/page.tsx
@@ -0,0 +1,9 @@
+type TicketProps = {
+  params: { id: string };
+};
+const Ticket = ({ params }: TicketProps) => {
+  console.log("Params", params);
+  return <h1>Tickets {params.id}</h1>;
+};
+
+export default Ticket;
