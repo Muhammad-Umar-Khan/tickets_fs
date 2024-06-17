da65407074d5d33925adf6fb4024aa5c0bc23c70
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-06-17 15:58:04 +0500

    icons config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
