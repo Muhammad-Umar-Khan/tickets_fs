22ac9b5194ba71132ad3c34a9402c9405d65f4fd
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-06-17 16:00:39 +0500

    config.autoAddCss = false

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core";
 
+config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
         "card-hover": "#4f5e74",
         "default-text": "#f1f3f5",
         "blue-accent": "#0084d4",
-        "blue-accent-hover": "#009fff",
+        "blue-accent-hover": "#009fff"
       },
     },
   },
