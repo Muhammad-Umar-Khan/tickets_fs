9d3422cc29a1c94725a2609e0572e243a11c0da6
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-06-20 20:16:35 +0500

    text-default-text

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-default-text`}>
+      <body className={`${inter.className}`}>
         <div className="flex flex-col h-screen max-h-screen">
           <Nav />
-          <div className="flex-grow overflow-y-auto bg-page">{children}</div>
+          <div className="flex-grow overflow-y-auto bg-page text-default-text">{children}</div>
         </div>
       </body>
     </html>
