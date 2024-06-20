a818e892169959f73089bd665994a630addb0f0a
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-06-20 12:05:38 +0500

    nav

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -4,14 +4,17 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav>
-      <div>
+    <nav className="flex justify-between bg-nav">
+      <div className="flex items-center space-x-4">
         <Link href="/">
           <FontAwesomeIcon icon={faHome} className="icon" />
         </Link>
         <Link href="ticket/new">
-          <FontAwesomeIcon icon={faTicket} />
+          <FontAwesomeIcon icon={faTicket} className="icon"/>
         </Link>
+      </div>
+      <div>
+        <p>umar@nineprimes.com</p>
       </div>
     </nav>
   );
