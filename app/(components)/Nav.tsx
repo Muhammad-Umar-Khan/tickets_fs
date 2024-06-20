6a796c0ebd3483ba2651bf31f73c07b50c3383b6
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-06-20 16:49:24 +0500

    icons

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="flex items-center justify-between bg-nav">
+    <nav className="flex items-center px-4 py-2 justify-between bg-nav">
       <div className="flex items-center space-x-4">
         <Link href="/">
           <FontAwesomeIcon icon={faHome} className="w-10 h-10" />
