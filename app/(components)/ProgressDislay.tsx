cf88a5f47ce5d80f61bd84b8eac56cdf2473ab9f
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-06-24 16:28:33 +0500

    width

--- app/(components)/ProgressDislay.tsx
+++ app/(components)/ProgressDislay.tsx
@@ -3,7 +3,7 @@ const ProgressDisplay = () => {
     <div className="w-full bg-gray-200 rounded-full h-2.5">
       <div
         className="bg-blue-600 rounded-full h-2.5"
-        style={{ width: "20%" }}
+        style={{ width: "75%" }}
       ></div>
     </div>
   );
