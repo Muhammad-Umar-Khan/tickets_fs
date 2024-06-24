6fec4ebe9a1dc2219f3eb8aaf621e76198876dd2
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-06-24 16:29:56 +0500

    line-width

--- app/(components)/PriorityDisplay.tsx
+++ app/(components)/PriorityDisplay.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PriorityDisplay = () => {
   return (
-    <div className="flex justify-start">
+    <div className="flex justify-start align-baseline">
       <FontAwesomeIcon icon={faFire} className="text-red-400 w-5 h-5" />
       <FontAwesomeIcon icon={faFire} className="text-red-400 w-5 h-5" />
       <FontAwesomeIcon icon={faFire} className="text-red-400 w-5 h-5" />
