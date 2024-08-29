9416ca5452c1af586f9be99e8127cf4db31a2e54
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-08-29 22:15:43 +0500

    loca

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -11,8 +11,6 @@ const Nav = () => {
         </Link>
         <Link href="ticket/new">
           <FontAwesomeIcon icon={faTicket} className="w-10 h-10" />
-          <FontAwesomeIcon icon={faTicket} className="w-10 h-10" />
-
         </Link>
       </div>
       <div>
