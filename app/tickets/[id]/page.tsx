ae05db5916d5d2309e848cb1095bdf4f994a0f2a
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-08-29 22:18:56 +0500

    searchParams

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,8 +1,9 @@
 type TicketProps = {
-  params: { id: string };
+  params: { id: string, q: string };
+  searchParams: {q: string}
 };
-const Ticket = ({ params }: TicketProps) => {
-  return <h1>Tickets {params.id}</h1>;
+const Ticket = ({ params, searchParams }: TicketProps) => {
+  return <h1>Tickets {params.id} and query is {searchParams.q}</h1>;
 };
 
 export default Ticket;
