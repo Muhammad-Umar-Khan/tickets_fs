fd982ae9a0f0638603149a75a5b8829e03b9dc10
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-08-31 22:35:13 +0500

    code

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -7,7 +7,7 @@ type TicketProps = {
 const Ticket = ({ params }: TicketProps) => {
   return (
     <h1>
-      Tickets {params.id} and
+      Tickets {params.id}
       <Link href="/" className="underline">
         Home
       </Link>
